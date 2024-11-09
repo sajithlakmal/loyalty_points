5d68710a6f45cfbe44074473acdcadddc49ef7cc
Sajith <sajithandlakmal100@gmail.com>
2024-11-09 12:53:07 +0530

    PointsRequest added

---
+++ src/main/java/com/abc/company/Model/PointsRequest.java
@@ -0,0 +1,13 @@
+package com.abc.company.Model;
+
+public class PointsRequest {
+    private int points;
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+}
